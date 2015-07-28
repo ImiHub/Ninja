21d68f27568ee299eea0241020a064c1f85d0b77
thief600 <thief600@gmail.com>
2015-07-28 13:38:00 +0200

    update 3

---
+++ NinjaWarrior/src/sem/game/entity/enemy/SimpleEnemy.java
@@ -0,0 +1,32 @@
+package sem.game.entity.enemy;
+
+import java.awt.Graphics;
+
+import sem.game.Handler;
+import sem.game.Id;
+import sem.game.entity.Entity;
+
+public class SimpleEnemy extends Entity
+{
+
+	public SimpleEnemy(int x, int y, int width, int height, Id id, Handler h)
+	{
+		super(x, y, width, height, id, h);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void render(Graphics g)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update()
+	{
+		// TODO Auto-generated method stub
+		
+	} 
+
+}
